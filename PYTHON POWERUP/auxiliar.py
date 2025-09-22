0c4b16c1aaf6c2e7eabc8a62ea8f02b3bc9cae74
Carlos Mendes <carlosmendesjmc@gmail.com>
2025-09-21 21:33:28 -0300

    Cadastrando os produtos automaticamente no formulário

--- PYTHON POWERUP/auxiliar.py
+++ PYTHON POWERUP/auxiliar.py
@@ -5,5 +5,12 @@ import time
 # Aguarda um tempo pra mover o mouse ate a posição que deseja captar 
 time.sleep(10)
 
+#Clicando 7x na na barra de scroll
+##pyautogui.click(x=1908, y=999, clicks=7)
+
+
+# Aguarda um tempo pra mover o mouse ate a posição que deseja captar 
+## time.sleep(10)
+
 #capta a posição do mouse e imprime no terminal
 print(pyautogui.position())
--- PYTHON POWERUP/codigo.py
+++ PYTHON POWERUP/codigo.py
@@ -36,7 +36,9 @@ pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
 pyautogui.press("enter")
 
 # Espere 3 Segundos
-time.sleep(3)
+time.sleep(2)
+
+# Passo 2: Fazer login
 
 # Fazer Login no site da empresa com as credencias e colocar senha
 # Clica na posiçao onde esta o campo que sera digitado o email
@@ -48,10 +50,10 @@ pyautogui.click(x=538, y=565)
 pyautogui.write("carlosmendesjmc@gmail.com")
 
 # Espere 3 Segundos
-time.sleep(3)
+time.sleep(2)
 
-# Clica na posiçao onde esta o campo que sera digitado a senha 
-pyautogui.click(x=539, y=705) 
+# Clica na posiçao onde esta o campo que sera digitado a senha
+pyautogui.click(x=539, y=705)
 
 # Digita a senha para logar
 pyautogui.write("12345")
@@ -60,29 +62,81 @@ pyautogui.write("12345")
 pyautogui.press("enter")
 
 # Espere 3 Segundos
-time.sleep(3)
+time.sleep(2)
+
+# Passo 3: Importar a base de dados que sera cadastrado no sistema da empresa
 
 # Importar a base de dados que vamos cadastrar
 # Criamos esta variavel tabela pra receber os dados que vamos ler
 
-tabela = pandas.read_csv("C:\Temp\JornadaPythonAutomacao\produtos.csv")
+tabela = pandas.read_csv("C:\\Temp\\JornadaPythonAutomacao\\produtos.csv")
 
 print(tabela)
 
 
-
-
-
-
-
-
-
-
-
-# Passo 2: Fazer login
-# Passo 3: Importar a base de dados que sera cadastrado no sistema da empresa
+# Posso usar, pyautogui.scroll(100) -100  pracima ou pra baixo
 # Passo 4: Cadastrar produto 1 no sistema
+
+for linha in tabela.index:  # Para cada linha da minha tabela ele vai percorrer e exucutar a mesma ação
+
+    # Código do Produto
+    pyautogui.click(x=615, y=390)
+    codigo = tabela.loc[linha, "codigo"]
+    pyautogui.write(codigo)
+
+
+    # Marca do Produto
+    pyautogui.press("tab")
+    marca = tabela.loc[linha, "marca"]
+    pyautogui.write(marca)
+
+    # Tipo do Produto
+    pyautogui.press("tab")
+    tipo = tabela.loc[linha, "tipo"]
+    pyautogui.write(tipo)
+
+    # Categoria do Produto
+    pyautogui.press("tab")
+    categoria = tabela.loc[linha, "categoria"]
+    pyautogui.write(str(categoria))
+
+    # Preço Unitário do Produto
+    pyautogui.press("tab")
+    preco = tabela.loc[linha, "preco_unitario"]
+    pyautogui.write(str(preco))
+
+    #Clicando 7x na na barra de scroll
+    pyautogui.click(x=1908, y=999, clicks=7)  # clicks=7 
+
+    # Custo do Produto
+    pyautogui.press("tab")
+    custo = tabela.loc[linha, "custo"]
+    pyautogui.write(str(custo))
+
+    # OBS
+    pyautogui.press("tab")
+    obs = (tabela.loc[linha, "obs"])
+    #if pandas.notna(obs):
+    #if obs != "nan":
+    
+    if pandas.isna(obs) or str(str).lower() == "nan":
+        pyautogui.write("obs")
+    else:
+        pyautogui.write(str(obs))
+        
+    pyautogui.press("tab")
+
+    # Clicando no botão de enviar/cadastrar
+    pyautogui.press("enter")
+    
+    pyautogui.sleep(1)
+    
+    pyautogui.scroll(1000)
+
+
 # passo 5: repetir o mesmo passo para cadastrar todos produtos no sistema da empresa
+
+# Para pausar a automação coloque o mouse no canto esquerdo da tela e aguarde é como fazer um FAILSAFE
 
 
 
